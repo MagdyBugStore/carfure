8ad244cace7fea6493894f44b136b85bf33468e2
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-06-10 23:08:33 +0300

    updaload server

--- index2.js
+++ index2.js
@@ -93,7 +93,7 @@ const fetchDataAndProcess = async () => {
     console.log('Connected to MongoDB');
     const db = client.db(dbName);
 
-    for (let productId = 30000; productId < 50000; productId++) {
+    for (let productId = 34442; productId < 50000; productId++) {
       try {
         const response = await axios(generateRequestOptions(productId));
         const productData = extractProductData(response.data);
